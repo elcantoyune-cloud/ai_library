ff05c6833e30dc42f92a10bd07f7753bd1d6fb84
elcantoyune-cloud <elcantoyune@gmail.com>
2026-06-24 13:26:28 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,7 @@
-self.addEventListener('install', () => {
-    console.log('sw installed');
+self.addEventListener('install', (event) => {
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(clients.claim());
 });
